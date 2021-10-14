69a9ec64b9a50508aa76a512321f3ed51edc09d4
Вадим Ермаков <vadimka_ermakov_3@mail.ru>
2021-10-14 11:03:42 +0400

    Добавьте файлы проекта.

---
+++ Ermakov_ISP31_LuckyTicket/Class_Checking.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ermakov_ISP31_LuckyTicket
+{
+    public class Class_Parsing
+    {
+        public static short StrToShortDef(string s, short @default)
+        {
+            short number;
+            if (short.TryParse(s, out number))
+                return number;
+            return @default;
+        }
+
+        public static void ConvertLongToArray(long lg_value, ref short[] sh_array)
+        {
+            for (short sh_i = 0; sh_i < 6; sh_i++)
+                sh_array[sh_i] = 0; // initial set to null 
+            string str = lg_value.ToString();
+            long count = str.Count(); // find count 
+            for (long lg_i = count - 1; lg_i >= 0; lg_i--)
+            {
+                string str_digit = (string)str.ElementAt((int)lg_i).ToString();
+                sh_array[lg_i] = StrToShortDef(str_digit, sh_array[lg_i]); // convert every digit 
+            }
+        }
+
+        public static string ConvertLongToString(long lg_value, short sh_num_of_digits)
+        {
+            string str_return = "";
+            string str = lg_value.ToString();
+            long count = str.Count(); // find count 
+            for (long lg_i = 0; lg_i < sh_num_of_digits - count; lg_i++)
+            {
+                str_return += "0"; // add null first 
+            }
+            for (long lg_i = 0; lg_i < count; lg_i++)
+            {                    // add values up to number of digits 
+                str_return += (string)str.ElementAt((int)lg_i).ToString();
+            }
+            return str_return;
+        }
+    }
+    public class Class_Evaluating
+    {
+        public const short DEF_LUCKY_TICKET = 1;
+        public const short DEF_USUAL_TICKET = 0;
+        public const short DEF_FAILED_TICKET = -1;
+        public const short DEF_VARIANT_DEFAULT = 100;
+        public const short DEF_VARIANT_TASK = 101;
+        //‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐ 
+        public static string GetMessageByLuckyValue(short sh_is_lucky)
+        {
+            switch (sh_is_lucky)
+            {
+                case DEF_LUCKY_TICKET:
+                    return "Получен счастливый билет!)";
+                case DEF_USUAL_TICKET:
+                    return "Получен обычный билет!(";
+                default:
+                    return "Задан недопустимый номер билета";
+            }
+        }
+
+        public static short IsHappyLucky(short[] arr, short sh_checked)
+        {
+            short sh_return = CheckInput(arr);
+            if (sh_return == DEF_USUAL_TICKET)
+            {
+                switch (sh_checked)
+                {
+                    case DEF_VARIANT_DEFAULT:
+                        sh_return = RevealLucky_Default(arr);
+                        break;
+                    case DEF_VARIANT_TASK:
+                        sh_return = RevealLucky_Task(arr);
+                        break;
+                }
+                return sh_return;
+            }
+            else
+            {
+                return sh_return;
+            }
+        }
+
+        //‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐ 
+        private static short CheckInput(short[] arr)
+        {
+            short iter = 0;
+            for (; iter <= 5; iter++)
+            {
+                if (arr[iter] < 0 || arr[iter] > 9)
+                {
+                    return DEF_FAILED_TICKET;
+                }
+            }
+            return DEF_USUAL_TICKET;
+        }
+        //‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐ 
+        private static short RevealLucky_Default(short[] arr)
+        {
+            short iter = 1;
+            for (; iter <= 5; iter++)
+            {
+                if (!
+                    (
+                       (arr[iter] % 2 == 0 ||
+                        arr[iter] % 3 == 0)
+                       &&
+                       (arr[iter] == arr[0])
+                    )
+                  )
+                {
+                    return DEF_USUAL_TICKET;
+                }
+            }
+            return DEF_LUCKY_TICKET;
+        }
+
+        //‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐ 
+        private static short RevealLucky_Task(short[] arr)
+        {
+            return DEF_USUAL_TICKET;
+        }
+    }
+}
+
---
+++ Ermakov_ISP31_LuckyTicket/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using evaluate = Ermakov_ISP31_LuckyTicket.Class_Evaluating;
+using parse = Ermakov_ISP31_LuckyTicket.Class_Parsing;
+
+namespace Ermakov_ISP31_LuckyTicket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_CheckVariant_Click(object sender, RoutedEventArgs e)
+        {
+            //prepare variables (объявляем необходимые переменные) 
+            short[] arr = { -1, -1, -1, -1, -1, -1 };
+            short def = -1;
+
+            // (извлекаем не более 6 символов из компонента TextBox в переменную arr) 
+
+            string str_arr = this.textBox_Input.Text;
+            long lg_count = str_arr.Count();
+            for (long lg_i = 0; lg_i < lg_count; lg_i++)
+            {
+                if (lg_i == 6)
+                    break;
+                string str = str_arr.ElementAt((int)lg_i).ToString();
+                arr[lg_i] = (short)parse.StrToShortDef(str, def);
+            }
+
+            // (устанавливаем индекс, соответствующий радиокнопке) 
+            short sh_checked = evaluate.DEF_VARIANT_DEFAULT;
+            if (radioButton_VariantTask.IsChecked == true)
+                sh_checked = evaluate.DEF_VARIANT_TASK;
+
+            // (проверяем на соответствие все 6 цифр) 
+            short sh_answer = evaluate.IsHappyLucky(arr, sh_checked);
+
+            // (получаем ответ в виде строки и выводим его пользователю)
+            string str_answer = evaluate.GetMessageByLuckyValue(sh_answer);
+            MessageBox.Show(str_answer);
+        }
+
+        private void button_FindAllLucky_Click(object sender, RoutedEventArgs e)
+        {
+            // (устанавливаем индекс, соответствующий радиокнопке) 
+            short sh_checked = evaluate.DEF_VARIANT_DEFAULT;
+            if (radioButton_VariantTask.IsChecked == true)
+                sh_checked = evaluate.DEF_VARIANT_TASK;
+
+            // (подготавливаем список, в который будет сохраняться выборка данных) 
+            List<string> lst_lucky = new List<string>();
+            lst_lucky.Clear();
+
+            // (подготавливаем массив для 6 цифр) 
+            short[] sh_arr = { -1, -1, -1, -1, -1, -1 };
+
+            // (создаем цикл, перебирающий все допустимые значения билета) 
+            long lg_count_i = 0;
+            for (long lg_i = 0; lg_i <= 999999; lg_i += 111111)
+            {
+                parse.ConvertLongToArray(lg_i, ref sh_arr);
+                short sh_answer = evaluate.IsHappyLucky(sh_arr, sh_checked);
+                if (sh_answer == evaluate.DEF_LUCKY_TICKET)
+                {
+                    string str = "[" + lg_count_i.ToString() + "] " + parse.ConvertLongToString(lg_i, 6);
+                    lst_lucky.Add(str);
+                    lg_count_i++;
+                }
+            }
+
+            // (обновляем содержимое компонента listbox) 
+            listbox_Enumerate.Items.Clear();
+            listbox_Enumerate.Items.Add(lst_lucky.ToArray());
+        }
+
+    }
+}
